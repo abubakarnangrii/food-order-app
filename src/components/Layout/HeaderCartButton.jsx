5384f5d279c7849a315f216eb8709a170b825470
abubakarnangrii <abubakarnangri@gmail.com>
2024-03-13 03:11:40 +0500

    modal is working

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,9 +1,9 @@
 import CartIcon from "../Cart/CartIcon"
 import CSS from './HeaderCartButton.module.css'
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
-    <button className={CSS.button}>
+    <button className={CSS.button} onClick={props.onClick}>
         <span className={CSS.icon}><CartIcon/></span>
         <span>My Cart</span>
         <span className={CSS.badge}>3</span>
